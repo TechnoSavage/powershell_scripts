45c218e8570fab1984441e9e00b7482abad85f80
TechnoSavage <technosavage@gmail.com>
2018-08-24 10:29:47 -0400

    Added new lines for readability.

--- lightbudget.ps1
+++ lightbudget.ps1
@@ -1,52 +1,52 @@
 function menu {
-  [int]$option = Read-Host 'What would you like to do:
+  [int]$option = Read-Host "`nWhat would you like to do:
   1 - Calculate the max allowed coupler loss for inserting a TAP into a link
   2 - Calculate the max allowed cable length for a given TAP split ratio in a link
   3 - Display ethernet fiber standards and max cabling distance
   4 - Exit
-  Enter your selection'
+  Enter your selection "
   If ($option -eq 1) {max_split}
   ElseIf ($option -eq 2) {max_cable}
   ElseIf ($option -eq 3) {ethernet_table}
   ElseIf ($option -eq 4) {Exit}
-  Else {Write-Host 'Invalid Option'
+  Else {Write-Host "`nInvalid Option"
         Exit}
 }
 
 function max_split {
-  $sender = Read-Host 'What is the sender transmit power (dB)'
-  $receiver = Read-Host 'What is the receiver sensitivity (dB)'
+  $sender = Read-Host "`nWhat is the sender transmit power (dB)"
+  $receiver = Read-Host "`nWhat is the receiver sensitivity (dB)"
   $link_loss_budget = $sender - $receiver
-  Write-Host "The Power Link Loss Budget for this link is $link_loss_budget dB."
-  [int]$mode = Read-Host 'Single-Mode or Multi-Mode fiber?
+  Write-Host "`nThe Power Link Loss Budget for this link is $link_loss_budget dB."
+  [int]$mode = Read-Host "`nSingle-Mode or Multi-Mode fiber?
   1 - Single-Mode
   2 - Multi-Mode
-  Enter the number of your selection '
-  $connectors = Read-Host 'How many connectors are in the path of the link?'
+  Enter the number of your selection "
+  $connectors = Read-Host "`nHow many connectors are in the path of the link?"
   If ($mode -eq 1) {
     $connector_loss = 0.2 * $connectors
     $mode_type = 'Single-Mode'
-    [int]$wave = Read-Host 'What is the wavelength being used?
+    [int]$wave = Read-Host "`nWhat is the wavelength being used?
     1 - 1310nm
     2 - 1550nm
-    Enter the number of your selection'
+    Enter the number of your selection "
     If ($wave -eq 1) {[int]$wavelength = 1310}
     Else {[int]$wavelength = 1550}
   }
   ElseIf ($mode -eq 2) {
     $connector_loss = 0.5 * $connectors
     $mode_type = 'Multi-Mode'
-    [int]$wave = Read-Host 'What is the wavelength being used?
+    [int]$wave = Read-Host "`nWhat is the wavelength being used?
     1 - 850nm
     2 - 1300nm
-    Enter the number of your selection'
+    Enter the number of your selection "
     If ($wave -eq 1) {[int]$wavelength = 850}
     Else {[int]$wavelength = 1300}
   }
-  Else {Write-Host 'That is not a valid input.'
+  Else {Write-Host "`nThat is not a valid input."
     menu}
-  Write-Host "The total loss introduced for the $mode_type link by connectors is $connector_loss dB."
-  [int]$cable = Read-Host 'What is the cable length, in meters, from the sender to the receiver'
+  Write-Host "`nThe total loss introduced for the $mode_type link by connectors is $connector_loss dB."
+  [int]$cable = Read-Host "`nWhat is the cable length, in meters, from the sender to the receiver?"
   If ($mode_type -eq 'Single-Mode' -and $wavelength -eq 1310) {
     $attenuation = 0.4}
   ElseIf ($mode_type -eq 'Single-Mode' -and $wavelength -eq 1550) {
@@ -55,22 +55,22 @@ function max_split {
     $attenuation = 3.0}
   ElseIf ($mode_type -eq 'Multi-Mode' -and $wavelength -eq 1300) {
     $attenuation = 1.0}
-  Else {Write-Host 'Something went wrong.'
+  Else {Write-Host "`nSomething went wrong."
     menu}
   $cable_loss = $cable / 1000 * $attenuation
-  Write-Host "The loss introduced by the length of cable for the $mode_type $wavelength nm link
+  Write-Host "`nThe loss introduced by the length of cable for the $mode_type $wavelength nm link
     is $cable_loss dB based on $attenuation dB/km fiber attenuation."
   $total_cable_loss = $connector_loss + $cable_loss
-  Write-Host "The total connection loss is $total_cable_loss dB."
+  Write-Host "`nThe total connection loss is $total_cable_loss dB."
   $allowable_loss = $link_loss_budget - $total_cable_loss
-  Write-Host "The allowable coupler loss for a TAP is a $allowable_loss dB maximum at the monitor port."
-  [int]$choice = Read-Host 'Reference which TAP insertion loss values?
+  Write-Host "`nThe allowable coupler loss for a TAP is a $allowable_loss dB maximum at the monitor port."
+  [int]$choice = Read-Host "`nReference which TAP insertion loss values?
   1 - Industry Standard
   2 - Cubro Average
-  Enter the number of your selection'
+  Enter the number of your selection "
   If ($choice -eq 1) {match_industry $mode_type $allowable_loss}
   ElseIf ($choice -eq 2) {match_cubro $mode_type $allowable_loss}
-  Else {Write-Host 'Invalid input'
+  Else {Write-Host "`nInvalid input."
     menu}
 }
 
@@ -105,8 +105,8 @@ function match_industry {
       }
     }
   }
-  Else {Write-Host 'Something went wrong.'}
-  Write-Host "The following split ratios are acceptable for this link $usable"
+  Else {Write-Host "`nSomething went wrong."}
+  Write-Host "`nThe following split ratios are acceptable for this link $usable."
 }
 
 function match_cubro {
@@ -140,33 +140,33 @@ function match_cubro {
       }
     }
   }
-  Else {Write-Host 'Something went wrong.'}
-  Write-Host "The following split ratios are acceptable for this link $usable"
+  Else {Write-Host "`nSomething went wrong."}
+  Write-Host "`nThe following split ratios are acceptable for this link $usable."
 }
 
 function max_cable {
-  $sender = Read-Host 'What is the sender transmit power (dB)'
-  $receiver = Read-Host 'What is the receiver sensitivity (dB)'
+  $sender = Read-Host "`nWhat is the sender transmit power (dB)"
+  $receiver = Read-Host "`nWhat is the receiver sensitivity (dB)"
   $link_loss_budget = $sender - $receiver
-  Write-Host "The Power Link Loss Budget for this link is $link_loss_budget dB."
-  [int]$mode = Read-Host 'Single-Mode or Multi-Mode fiber?
+  Write-Host "`nThe Power Link Loss Budget for this link is $link_loss_budget dB."
+  [int]$mode = Read-Host "`nSingle-Mode or Multi-Mode fiber?
   1 - Single-Mode
   2 - Multi-Mode
-  Enter the number of your selection '
+  Enter the number of your selection "
   $valid_input = 1, 2
   If ($mode -notin $valid_input) {
-    Write-Host "$mode is not a valid input."
+    Write-Host "`n$mode is not a valid input."
     menu}
-  $connectors = Read-Host 'How many connectors are in the path of the link?'
+  $connectors = Read-Host "`nHow many connectors are in the path of the link?"
   If ($mode -eq 1) {
     $connector_loss = 0.2 * $connectors
     $mode_type = 'Single-Mode'
-    [int]$wave = Read-Host 'What is the wavelength being used?
+    [int]$wave = Read-Host "`nWhat is the wavelength being used?
     1 - 1310nm
     2 - 1550nm
-    Enter the number of your selection'
+    Enter the number of your selection "
     If ($wave -notin $valid_input) {
-      Write-Host "$wave is not a valid option."
+      Write-Host "`n$wave is not a valid option."
       menu}
     If ($wave -eq 1) {[int]$wavelength = 1310}
     Else {[int]$wavelength = 1550}
@@ -174,29 +174,29 @@ function max_cable {
   ElseIf ($mode -eq 2) {
     $connector_loss = 0.5 * $connectors
     $mode_type = 'Multi-Mode'
-    [int]$wave = Read-Host 'What is the wavelength being used?
+    [int]$wave = Read-Host "`nWhat is the wavelength being used?
     1 - 850nm
     2 - 1300nm
-    Enter the number of your selection'
+    Enter the number of your selection "
     If ($wave -notin $valid_input) {
-      Write-Host "$wave is not a valid option."
+      Write-Host "`n$wave is not a valid option."
       menu}
     If ($wave -eq 1) {[int]$wavelength = 850}
     Else {[int]$wavelength = 1300}
   }
-  Else {Write-Host "$mode is not a valid input."
+  Else {Write-Host "`n$mode is not a valid input."
     menu}
-  Write-Host "The total loss introduced for the $mode_type link by connectors is $connector_loss dB."
-  [int]$split = Read-Host 'What is the split ratio of the TAP?
+  Write-Host "`nThe total loss introduced for the $mode_type link by connectors is $connector_loss dB."
+  [int]$split = Read-Host "`nWhat is the split ratio of the TAP?
     1 - 50/50
     2 - 60/40
     3 - 70/30
     4 - 80/20
     5 - 90/10
-    Enter the number of your selection '
+    Enter the number of your selection "
   $valid_split = 1, 2, 3, 4, 5
   If ($split -notin $valid_split) {
-    Write-Host "$split is not a valid option."
+    Write-Host "`n$split is not a valid option."
     menu}
   $split_ratios=[ordered]@{1='50/50'
                            2='60/40'
@@ -232,7 +232,7 @@ function max_cable {
       }
     }
   }
-  Else {Write-Host 'Something went wrong.'
+  Else {Write-Host "`nSomething went wrong."
     menu}
   $total_loss_net = $link_loss_budget - $connector_loss - $network
   $total_loss_mon = $link_loss_budget - $connector_loss - $monitor
@@ -248,7 +248,7 @@ function max_cable {
   Elseif ($mode_type -eq 'Multi-Mode' -and $wavelength -eq 1300) {
       $attenuation = 1.0
   }
-  Else {Write-Host 'Something went wrong.'}
+  Else {Write-Host "`nSomething went wrong."}
   $cable_net = 1
   $cable_loss_net = $cable_net * $attenuation/1000
   while ($total_loss_net - $cable_loss_net -gt 0) {
@@ -271,15 +271,15 @@ function cable_by_eth_standard {
          [int]$cable_mon)
   $valid_input = 1, 2, 3, 4, 5
   If ($mode_type -eq 'Multi-Mode') {
-    [int]$standard_type = Read-Host 'What is the Ethernet Standard in use?
+    [int]$standard_type = Read-Host "`nWhat is the Ethernet Standard in use?
         1 - OM1-SX
         2 - OM1-LX
         3 - OM2
         4 - OM3
         5 - OM4
-        Enter the number of your selection '
+        Enter the number of your selection "
     If ($standard_type -notin $valid_input) {
-      Write-Host "$standard_type is not a valid input."
+      Write-Host "`n$standard_type is not a valid input."
       menu
     }
     $standard_table = [ordered]@{1='OM1-SX'
@@ -289,15 +289,15 @@ function cable_by_eth_standard {
                                  5='OM4'}
     $standard = $standard_table[$standard_type]
   }
-  [int]$speed = Read-Host 'What speed is the link?
+  [int]$speed = Read-Host "`nWhat speed is the link?
     1 - 100M
     2 - 1G
     3 - 10G
     4 - 40G
     5 - 100G
-    Enter the number of your selection '
+    Enter the number of your selection "
   If ($speed -notin $valid_input) {
-    Write-Host "$speed is not a valid input."
+    Write-Host "`n$speed is not a valid input."
     menu
   }
   $speed_table = [ordered]@{1='100M'
@@ -348,16 +348,16 @@ function cable_by_eth_standard {
       $cable_net = $max_standard_length}
     If ($cable_mon -gt $max_standard_length) {
       $cable_mon = $max_standard_length}
-  Write-Host "The maximum combined cable length from sender to TAP and from
-              TAP to receiver is $cable_net meters"
-  Write-Host "The maximum combined cable length from sender to TAP and from
-              TAP monitor to tool is $cable_mon meters"
+  Write-Host "`nThe maximum combined cable length from sender to TAP and from
+              TAP to receiver is $cable_net meters."
+  Write-Host "`nThe maximum combined cable length from sender to TAP and from
+              TAP monitor to tool is $cable_mon meters."
   menu
   }
 }
 
 function ethernet_table {
-  Write-Host 'Ethernet Fiber Standards and max cabling distance:
+  Write-Host "`nEthernet Fiber Standards and max cabling distance:
      ________________________________________________________________________________________________________________
     |         |    Core/   |      | FastEthernet |  1G Ethernet  |  1G Ethernet  |    10G    |    40G    |    100G   |
     |  Name   |  Cladding  | Type |  100BaseFX   |  1000Base-SX  |  1000Base-LX  |  10GBase  |  40GBase  |  100GBase |
@@ -374,7 +374,7 @@ function ethernet_table {
     |   SM    |   9/125    |  SM  |    2000M     |    1310nm     |    1310nm     |  1310nm   |           |           |
     |_________|____________|______|______________|_______________|_______________|___________|___________|___________|
     *mode condition patch cable required
-    '
+    "
     menu
 }
 
