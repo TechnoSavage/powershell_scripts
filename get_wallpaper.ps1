208b7b5ee99e8294e04b6b350acf58b1a94e6573
TechnoSavage <technosavage@gmail.com>
2018-10-18 11:43:44 -0400

    .

---
+++ get_wallpaper.ps1
@@ -0,0 +1,22 @@
+Write-Host "`nThis script will append .jpg to all files in a specified directory
+and discard any images smaller than 1920x1080 in size.`n"
+[string]$folder = Read-Host 'Enter the path to the directory [C:\Users\*youruser*\Desktop\New folder]'
+If ($folder -eq "") {
+  $folder = "C:\Users\$env:Username\Desktop\New folder"
+}
+$exists = Test-Path $folder
+If ($exists) {
+  Set-Location $folder
+  $file_list = Get-ChildItem
+  Write-Host $file_list
+  [string]$confirm = Read-Host 'Are these the files you wish to change the file extension for? [y/n]'
+  If ('y', 'yes' -contains $confirm.ToLower()) {
+    foreach ($item in $file_list) {
+      $old_name = $item.Name
+      $new_name = $item.Name + '.jpg'
+      Rename-Item $old_name $new_name
+    }
+  }
+  Else {Write-Host 'No changes were made.'}
+}
+Else {Write-Host 'That directory does not exist'}
--- profile_functions.psm1
+++ profile_functions.psm1
@@ -5,7 +5,9 @@ function Compare-Hash {
 			[string]$provided_hash,
 			[string]$filename)
 		$calculated = Get-FileHash $filename -a $algorithm
-		Write-Host $calculated
+		Write-Host "`nFile hashed is" $calculated.Path
+		Write-Host "Hash is" $calculated.Hash
+		Write-Host "Hash algorithm is" $calculated.Algorithm
 		Write-Host "Do hashes match: "
 		$test = $provided_hash -eq $calculated.Hash
 		Write-Host $test
